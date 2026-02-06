48de74e29dde7d78167e69589edd7b2f5243d34c
Neil Macneale V <neil.macneale.v@gmail.com>
2026-02-06 11:53:20 -0800

    Make settings public

--- fl-sim/src/lib.rs
+++ fl-sim/src/lib.rs
@@ -1,17 +1,23 @@
-use nalgebra::{Point3, Vector3};
+use nalgebra::{Point3, Vector3, vector};
+
+use crate::space::SpatialIndex;
 
 mod space;
 
-pub struct Simulation {
-  delta_time:       f32,
-  smoothing_length: f32,
-  rest_density:     f32,
-  iterations:       u32,
-  constraint:       f32,
-  viscosity:        f32,
+pub struct Settings {
+  pub delta_time:       f32,
+  pub smoothing_length: f32,
+  pub rest_density:     f32,
+  pub iterations:       u32,
+  pub constraint:       f32,
+  pub viscosity:        f32,
+}
 
+pub struct Simulation {
+  settings:  Settings,
   size:      Vector3<f32>,
   particles: Vec<Particle>,
+  index:     SpatialIndex,
 }
 
 struct Particle {
@@ -19,4 +25,17 @@ struct Particle {
   velocity: Vector3<f32>,
   density:  f32,
   pressure: f32,
+}
+
+impl Simulation {
+  pub fn new(size: Vector3<f32>, settings: Settings) -> Simulation {
+    Simulation {
+      settings,
+      size,
+      particles: vec![],
+      index: SpatialIndex::new(size, vector![10, 10, 10]),
+    }
+  }
+
+  pub fn tick(&mut self) {}
 }
